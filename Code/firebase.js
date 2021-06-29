1d4433c70762dd31f84ca545e9efbdc65921feb6
Sneha4321 <71457762+Sneha4321@users.noreply.github.com>
2021-06-29 16:35:20 -0500

    First FrameWork

---
+++ Code/firebase.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBX4FBD6TKihqx2NW0CJa3SQFlaQvZpDhU",
+    authDomain: "e-article.firebaseapp.com",
+    databaseURL: "https://e-article.firebaseio.com",
+    projectId: "e-article",
+    storageBucket: "e-article.appspot.com",
+    messagingSenderId: "749741943751",
+    appId: "1:749741943751:web:e0759301a919b2c5"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
